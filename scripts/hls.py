99fe2aa2645e64561c0b627d36b632cbfeca921c
lucas luz feitosa <lucasluz@fortalsistemas.com.br>
2020-09-10 12:06:09 -0300

    script para gerar hls de video pelo id

--- scripts/hls.py
+++ scripts/hls.py
@@ -6,6 +6,7 @@ import mysql.connector
 from datetime import datetime
 import shutil
 import boto3
+from utils.error_handler import NotFoundException
 
 
 #############################################################################
@@ -28,8 +29,21 @@ HLS_BUCKET_NAME = os.environ.get('HLS_BUCKET_NAME')
 #                             FUNCTIONS                                     #
 #############################################################################
 def main(id: str):
-    # TODO Retrive download url from database based on video ID
-    download_url = 'url'
+    try:
+        date = datetime.utcnow()
+        connection = mysql.connector.connect(**DATABASE_INFO)
+        select_sql = """SELECT url fom video WHERE id = %s"""
+        cursor = connection.cursor()
+        cursor.execute(select_sql, (id))
+        result = cursor.fetchone()
+        cursor.close()
+    except mysql.connector.Error as error:
+        print(f'@@@@@@@@@@ Erro obter url do video: {str(error)}')
+
+    if not result:
+        raise NotFoundException(message='Video nao encontrado')
+
+    download_url = result[0]
 
     while not os.path.exists(f'videos/{id}'):
         os.makedirs(f'videos/{id}')
@@ -56,24 +70,24 @@ def main(id: str):
     os.system(f'ffmpeg -y -i videos/{id}/video.mp4 -hls_time 9 -hls_key_info_file videos/{id}/enc.keyinfo -hls_playlist_type vod -hls_segment_filename "videos/{id}/fileSequence%d.ts" videos/{id}/prog_index.m3u8'.format(id=id))
 
     # NOTE Save info on database
-    # try:
-    #     date = datetime.utcnow()
-    #     connection = mysql.connector.connect(**DATABASE_INFO)
-    #     insert_sql = """INSERT INTO hls_videos (video_id, `key`, iv, created_at, removed) VALUES (%s, %s, %s, %s, %s)"""
-    #     cursor = connection.cursor()
-    #     insert_tuple = (id, asciiBin, iv, str(date), False)
-    #     result = cursor.execute(insert_sql, insert_tuple)
-    #     connection.commit()
-    #     cursor.close()
-    # except mysql.connector.Error as error:
-    #     print(f'@@@@@@@@@@ Erro ao inserir video: {str(error)}')
+    try:
+        date = datetime.utcnow()
+        connection = mysql.connector.connect(**DATABASE_INFO)
+        insert_sql = """INSERT INTO hls_videos (video_id, `key`, iv, created_at, removed) VALUES (%s, %s, %s, %s, %s)"""
+        cursor = connection.cursor()
+        insert_tuple = (id, asciiBin, iv, str(date), False)
+        cursor.execute(insert_sql, insert_tuple)
+        connection.commit()
+        cursor.close()
+    except mysql.connector.Error as error:
+        print(f'@@@@@@@@@@ Erro ao inserir video: {str(error)}')
 
     # NOTE Remove video file before upload hls files to S3
     os.remove('videos/{id}/video.mp4')
     print('>>>>>>>>>>>> arquivo de video removido do diretorio')
 
     # NOTE Copy folder on local path to s3
-    for root, dirs, files in os.walk("videos/"):
+    for root, _, files in os.walk("videos/"):
         for name in files:
             dir_path = root + name
             response = s3_client.upload_file(dir_path, bucket_name, dir_path)
